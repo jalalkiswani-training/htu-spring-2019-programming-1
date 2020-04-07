5a66c11edcba381b65f90f28c5a44f506725697f
Jalal Kiswani <kiswanij@gmail.com>
2020-04-07 09:48:09 +0300

    .

--- src/main/java/edu/htu/lesson4/LinearSearch.java
+++ src/main/java/edu/htu/lesson4/LinearSearch.java
@@ -6,22 +6,28 @@ import java.util.Random;
 
 public class LinearSearch {
 	public static void main(String[] args) {
-		for (int i = 1_000_000; i < 10_000_000; i+=1_000_0000) {
-			System.out.println("===========================================");
-			System.out.println("Working on N= "+i);
+			timeApplicationOnValue(1000);
+			timeApplicationOnValue(10_000);
+			timeApplicationOnValue(100_000);
+			timeApplicationOnValue(1_000_000);
+			timeApplicationOnValue(2_000_000);
+	}
+
+	private static void timeApplicationOnValue(int n) {
+		System.out.println("===========================================");
+		System.out.println("Working on N= "+n);
 //			int n = 700000000;
-			int arr[] = new int[i];// construction of array
+		int arr[] = new int[n];// construction of array
 
-			long start = System.currentTimeMillis();
-			fillArrayWithRandomValues(arr);
-			long end = System.currentTimeMillis();
+		long start = System.currentTimeMillis();
+		fillArrayWithRandomValues(arr);
+		long end = System.currentTimeMillis();
 
-			System.out.println("Random numbers generation took:" + (end - start));
-			printArrayValues(arr);
-			findIndex(arr, 5);
-			findIndex(arr, 50);
-			findIndex(arr, 200);
-		}
+		System.out.println("Random numbers generation took:" + (end - start));
+		printArrayValues(arr);
+		findIndex(arr, 5);
+		findIndex(arr, 50);
+		findIndex(arr, 200);
 	}
 
 	/////////////////////////////////////////////////////////////////
@@ -45,7 +51,7 @@ public class LinearSearch {
 	// paramaters
 	// return value
 	public static long findIndex(int arr[], int value) {
-		long start = System.currentTimeMillis();
+		Instant start=Instant.now();
 		int index = -1;
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == value) {
@@ -57,9 +63,10 @@ public class LinearSearch {
 //		if(index==-1) {
 ////			System.out.println("Value "+value+" not found");
 //		}
-		long end = System.currentTimeMillis();
-//		long duration = Duration.between(start, end).getSeconds();
-		long duration = end - start;
+		Instant end=Instant.now();
+//		long end = System.currentTimeMillis();
+		long duration = Duration.between(start, end).getNano();
+//		long duration = end - start;
 		System.out.println("Value " + value + " Index " + index + " Duration : " + duration);
 		return index;
 	}
